0dad45ac41e22fd09901d4af0b379fe0c5af1683
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2017-05-24 17:48:02 +0800

    Correct format IF ELSE statments format and spacing

--- refactor/app/Http/Controllers/BookingController.php
+++ refactor/app/Http/Controllers/BookingController.php
@@ -39,9 +39,7 @@ class BookingController extends Controller
 
             $response = $this->repository->getUsersJobs($user_id);
 
-        }
-        elseif($request->__authenticatedUser->user_type == env('ADMIN_ROLE_ID') || $request->__authenticatedUser->user_type == env('SUPERADMIN_ROLE_ID'))
-        {
+        } elseif($request->__authenticatedUser->user_type == env('ADMIN_ROLE_ID') || $request->__authenticatedUser->user_type == env('SUPERADMIN_ROLE_ID'))  {
             $response = $this->repository->getAll($request);
         }
 
@@ -201,11 +199,13 @@ class BookingController extends Controller
         } else {
             $distance = "";
         }
+        
         if (isset($data['time']) && $data['time'] != "") {
             $time = $data['time'];
         } else {
             $time = "";
         }
+
         if (isset($data['jobid']) && $data['jobid'] != "") {
             $jobid = $data['jobid'];
         }
